e2948f0b34620fb2bd85ae8950c9089df5463cea
badgersky <szymonek.ptasznik@gmail.com>
2023-06-20 16:53:32 +0200

    configured grid for new_players frame, create form for adding new player

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ class App(ctk.CTk):
         # grid configuration
         self.rowconfigure(0, weight=1)
         self.rowconfigure(0, weight=2)
-        self.columnconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)   
 
         # frames
         self.new_players = ctk.CTkFrame(self)
@@ -22,3 +22,16 @@ class App(ctk.CTk):
 
         self.new_players.grid(row=0, column=0, pady=10, padx=10, sticky='nsew')
         self.generator.grid(row=1, column=0, padx=10, pady=10, sticky='nsew')
+
+        self.new_players.columnconfigure((0, 2), weight=1)
+        self.new_players.columnconfigure(1, weight=2)
+
+        # adding new players
+        self.lbl_new_players = ctk.CTkLabel(self.new_players, text='Add new players:')
+        self.lbl_new_players.grid(row=0, column=0, padx=5, pady=5, sticky='w')
+
+        self.ent_new_players = ctk.CTkEntry(self.new_players)
+        self.ent_new_players.grid(row=0, column=1, padx=5, pady=5, sticky='ew')
+
+        self.btn_new_players = ctk.CTkButton(self.new_players, width=40, text='Add')
+        self.btn_new_players.grid(row=0, column=2, padx=5, pady=5)
